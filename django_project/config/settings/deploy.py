ad1d5e1de803085c2600e96f0e552747bcd0417d
pando <ndjman7@gmail.com>
2018-11-04 16:11:50 +0900

    fix: Fix nginx.conf

--- django_project/config/settings/deploy.py
+++ django_project/config/settings/deploy.py
@@ -3,12 +3,12 @@ from .common import *
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_DATA).read())
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config_secret_deploy['DJANGO']['ALLOWED_HOSTS']
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = config_secret_deploy['DATABASES']
+DATABASES = config_secret_deploy['DJANGO']['DATABASES']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
