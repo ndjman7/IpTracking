cfe0e77a712afffda699f127e4d4d9c2ab970a8e
pando <ndjman7@gmail.com>
2018-11-04 14:35:53 +0900

    feat: Add deploy files

--- django_project/config/settings/common.py
+++ django_project/config/settings/common.py
@@ -107,3 +107,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
--- django_project/config/settings/deploy.py
+++ django_project/config/settings/deploy.py
@@ -8,12 +8,7 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = config_secret_deploy['DATABASES']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
